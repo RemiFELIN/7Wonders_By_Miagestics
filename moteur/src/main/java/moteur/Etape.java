70c6da6f850fd15c5eaf5069084dea1eb04418a5
Felin <remi.felin@gmail.com>
2019-03-26 16:39:18 +0100

    Ajout etape.java et modification merveille.java

---
+++ moteur/src/main/java/moteur/Etape.java
@@ -0,0 +1,25 @@
+package moteur;
+
+public class Etape {
+
+    private int pointVictoire;
+    private int pointMilitaire;
+    private int piece;
+    private Ressource[] ressources;
+    private String effet;
+
+    Etape(){}
+
+    public void setCoup(Ressource[] ressources){
+        this.ressources = ressources;
+    }
+
+    public void setBonus(int pointVictoire, int pointMilitaire, int piece, Ressource[] ressources, String effet){
+        this.pointVictoire = pointVictoire;
+        this.pointMilitaire = pointMilitaire;
+        this.piece = piece;
+        this.ressources = ressources;
+        this.effet = effet;
+    }
+
+}
--- moteur/src/main/java/moteur/Merveille.java
+++ moteur/src/main/java/moteur/Merveille.java
@@ -2,115 +2,126 @@ package moteur;
 
 import java.util.ArrayList;
 
+import static moteur.Ressource.*;
+
 public class Merveille {
 
     private char face;
     private String nom;
     private Ressource res;
 
-    private Ressource[][] coutEtape;
-    private Ressource[][] bonusEtapeRes;
-    private int[] bonusEtapePv;
-    private int[] bonusEtapeMilitaire;
-    private int[] bonusEtapePiece;
-    private String[] bonusEtapeEffect;
+    private Etape[] etapes;
+    private int etapesConstruites = 0;
 
-    public Merveille(String nom, char face, Ressource res) {
+    public Merveille(String nom, char face, Ressource res, int nbEtapes) {
         this.nom = nom;
         this.face = face;
         this.res = res;
-        if(face == 'A'){
-            int[] array = new int[] { 3,0,7 };
-            setBonusEtapePv(array);
-        }
+        etapes = new Etape[nbEtapes];
     }
-
-    public final void setCoupEtape(Ressource[][] res){ this.coutEtape = res; }
-    public final void setBonusEtapePv(int[] pv){ this.bonusEtapePv = pv; }
-    public final void setBonusEtapeMilitaire(int[] militaire){ this.bonusEtapeMilitaire = militaire; }
-    public final void setBonusEtapePiece(int[] Piece){ this.bonusEtapePiece = Piece; }
-    public final void setBonusEtapeEffect(String[] Effect){ this.bonusEtapeEffect = Effect; }
-    public final void setBonusEtapeRes(Ressource[][] res){ this.bonusEtapeRes = res; }
 
     public final char getFace(){ return face; }
     public final String getNom(){ return nom; }
     public final Ressource getRessource(){ return res; }
 
-    public final Ressource[][] getCoutEtape(){ return coutEtape; }
-    public final Ressource[][] getBonusEtapeRes(){ return bonusEtapeRes; }
-    public final int[] getBonusEtapePv(){ return bonusEtapePv; }
-    public final int[] getBonusEtapeMilitaire(){ return bonusEtapeMilitaire;}
-    public final int[] getBonusEtapePiece(){ return bonusEtapePiece; };
-    public final String[] getBonusEtapeEffect(){ return bonusEtapeEffect; };
+    public void ajouterEtape(Etape etape, int indice){
+        etapes[indice] = etape;
+    }
 
     public final static ArrayList<Merveille> getPlateau(){
         ArrayList<Merveille> plateaux = new ArrayList<Merveille>();
         Merveille m;
+        Etape etape;
 
         //Plateau: Le Colosse de Rhodes
-        m = new Merveille("Le Colosse de Rhodes", 'A', Ressource.MINERAI);
-        Ressource[][] ressources = new Ressource[][]{new Ressource[]{Ressource.BOIS,Ressource.BOIS}
-        ,new Ressource[]{Ressource.ARGILE, Ressource.ARGILE,Ressource.ARGILE}
-        ,new Ressource[]{Ressource.MINERAI, Ressource.MINERAI,Ressource.MINERAI,Ressource.MINERAI}};
-        m.setCoupEtape(ressources);
-        int[] mil = new int[]{0,2,0};
-        m.setBonusEtapeMilitaire(mil);
+        m = new Merveille("Le Colosse de Rhodes", 'A', MINERAI, 3);
+            //etape 1
+            etape = new Etape();
+            etape.setCoup(new Ressource[]{BOIS, BOIS});
+            etape.setBonus(3, 0, 0, new Ressource[]{}, "");
+            m.ajouterEtape(etape, 1);
+            //etape 2
+            etape = new Etape();
+            etape.setCoup(new Ressource[]{ARGILE, ARGILE, ARGILE});
+            etape.setBonus(0, 2, 0, new Ressource[]{}, "");
+            m.ajouterEtape(etape, 2);
+            //etape 3
+            etape = new Etape();
+            etape.setCoup(new Ressource[]{MINERAI, MINERAI, MINERAI, MINERAI});
+            etape.setBonus(7, 0, 0, new Ressource[]{}, "");
+            m.ajouterEtape(etape, 3);
+        plateaux.add(m);
+
+        m = new Merveille("Le Colosse de Rhodes", 'B', MINERAI, 2);
+            //etape 1
+            etape = new Etape();
+            etape.setCoup(new Ressource[]{PIERRE, PIERRE, PIERRE});
+            etape.setBonus(3, 1, 3, new Ressource[]{}, "");
+            m.ajouterEtape(etape, 1);
+            //etape 2
+            etape = new Etape();
+            etape.setCoup(new Ressource[]{MINERAI, MINERAI, MINERAI, MINERAI});
+            etape.setBonus(4, 1, 4, new Ressource[]{}, "");
+            m.ajouterEtape(etape, 2);
         plateaux.add(m);
 
         //Plateau: Le phare d’Alexandrie
-        m = new Merveille("Le phare d’Alexandrie", 'A', Ressource.VERRE);
-        ressources = new Ressource[][]{new Ressource[]{Ressource.PIERRE, Ressource.PIERRE}
-        ,new Ressource[]{Ressource.MINERAI, Ressource.MINERAI}
-        ,new Ressource[]{Ressource.VERRE, Ressource.VERRE}};
+        m = new Merveille("Le phare d’Alexandrie", 'A', VERRE, 3);
+            //etape 1
+            etape = new Etape();
+                    /*
+        ressources = new Ressource[][]{new Ressource[]{PIERRE, PIERRE}
+        ,new Ressource[]{MINERAI, MINERAI}
+        ,new Ressource[]{VERRE, VERRE}};
         m.setCoupEtape(ressources);
-        ressources = new Ressource[][]{new Ressource[]{Ressource.ARGILE, Ressource.BOIS,Ressource.MINERAI,Ressource.MINERAI}};
-        m.setBonusEtapeRes(ressources);
+        ressources = new Ressource[][]{new Ressource[]{ARGILE, BOIS, MINERAI, MINERAI}};
+        //m.setBonusEtapeRes(ressources);
         plateaux.add(m);
 
         //Plateau: Le temple d'Artémis à Ephèse
-        m = new Merveille("Le temple d’Artémis à Ephèse", 'A', Ressource.PAPYRUS);
-        ressources = new Ressource[][]{new Ressource[]{Ressource.PIERRE, Ressource.PIERRE}
-        ,new Ressource[]{Ressource.BOIS, Ressource.BOIS}
-        ,new Ressource[]{Ressource.PAPYRUS, Ressource.PAPYRUS}};
+        m = new Merveille("Le temple d’Artémis à Ephèse", 'A', PAPYRUS, 3);
+        ressources = new Ressource[][]{new Ressource[]{PIERRE, PIERRE}
+        ,new Ressource[]{BOIS, BOIS}
+        ,new Ressource[]{PAPYRUS, PAPYRUS}};
         m.setCoupEtape(ressources);
         int[] piece = new int[] { 0,9,0 };
-        m.setBonusEtapePiece(piece);
+        //m.setBonusEtapePiece(piece);
         plateaux.add(m);
 
         //Plateau: Les jardins suspendus de Babylone
-        m = new Merveille("Les jardins suspendus de Babylone", 'A', Ressource.ARGILE);
-        ressources = new Ressource[][]{new Ressource[]{Ressource.ARGILE, Ressource.ARGILE}
-        ,new Ressource[]{Ressource.BOIS, Ressource.BOIS,Ressource.BOIS, Ressource.BOIS}
-        ,new Ressource[]{Ressource.ARGILE, Ressource.ARGILE,Ressource.ARGILE, Ressource.ARGILE}};
+        m = new Merveille("Les jardins suspendus de Babylone", 'A', ARGILE, 3);
+        ressources = new Ressource[][]{new Ressource[]{ARGILE, ARGILE}
+        ,new Ressource[]{BOIS, BOIS, BOIS, BOIS}
+        ,new Ressource[]{ARGILE, ARGILE, ARGILE, ARGILE}};
         m.setCoupEtape(ressources);
         plateaux.add(m);
 
         //Plateau: La statue de Zeus à Olympie
-        m = new Merveille("La statue de Zeus à Olympie", 'A', Ressource.BOIS);
-        ressources = new Ressource[][]{new Ressource[]{Ressource.BOIS, Ressource.BOIS}
-        ,new Ressource[]{Ressource.PIERRE, Ressource.PIERRE}
-        ,new Ressource[]{Ressource.MINERAI, Ressource.MINERAI}};
+        m = new Merveille("La statue de Zeus à Olympie", 'A', BOIS, 3);
+        ressources = new Ressource[][]{new Ressource[]{BOIS, BOIS}
+        ,new Ressource[]{PIERRE, PIERRE}
+        ,new Ressource[]{MINERAI, MINERAI}};
         m.setCoupEtape(ressources);
         plateaux.add(m);
 
         //Plateau: Le mausolée d’Halicarnasse
-        m = new Merveille("Le mausolée d’Halicarnasse", 'A', Ressource.TEXTILE);
-        ressources = new Ressource[][]{new Ressource[]{Ressource.ARGILE, Ressource.ARGILE}
-        ,new Ressource[]{Ressource.MINERAI, Ressource.MINERAI,Ressource.MINERAI}
-        ,new Ressource[]{Ressource.TEXTILE, Ressource.TEXTILE}};
+        m = new Merveille("Le mausolée d’Halicarnasse", 'A', TEXTILE, 3);
+        ressources = new Ressource[][]{new Ressource[]{ARGILE, ARGILE}
+        ,new Ressource[]{MINERAI, MINERAI, MINERAI}
+        ,new Ressource[]{TEXTILE, TEXTILE}};
         m.setCoupEtape(ressources);
         plateaux.add(m);
 
         //Plateau: La grande pyramide de Gizeh
-        m = new Merveille("La grande pyramide de Gizeh", 'A', Ressource.PIERRE);
-        ressources = new Ressource[][]{new Ressource[]{Ressource.PIERRE, Ressource.PIERRE}
-        ,new Ressource[]{Ressource.BOIS, Ressource.BOIS,Ressource.BOIS}
-        ,new Ressource[]{Ressource.PIERRE, Ressource.PIERRE,Ressource.PIERRE,Ressource.PIERRE}};
+        m = new Merveille("La grande pyramide de Gizeh", 'A', PIERRE, 3);
+        ressources = new Ressource[][]{new Ressource[]{PIERRE, PIERRE}
+        ,new Ressource[]{BOIS, BOIS, BOIS}
+        ,new Ressource[]{PIERRE, PIERRE,PIERRE,PIERRE}};
         m.setCoupEtape(ressources);
         int[] pv = new int[] { 3,5,7 };
-        m.setBonusEtapePv(pv);
+        //m.setBonusEtapePv(pv);
         plateaux.add(m);
         
-        return plateaux;
+        return plateaux;*/
     }
 }
